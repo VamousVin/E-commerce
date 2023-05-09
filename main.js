665fd9fc23cbadc42400449aefc4a27886e4a6ad
VamousVin <gamersgg568@gmail.com>
2023-05-09 10:28:56 +0800

    update navbar

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
+// Dropdown button
 const menu = document.querySelector("#menuDrop");
 const hamburger = document.querySelector("#hamburger");
 
 hamburger.addEventListener("click", () => {
   menu.classList.toggle("block");
+});
+
+document.body.addEventListener("click", (e) => {
+  if (!hamburger.contains(e.target) && !menu.contains(e.target)) {
+    menu.classList.remove("block");
+  }
 });
