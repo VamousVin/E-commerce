b53429ad8779119a47b6be32a5e28b0be227afd5
VamousVin <gamersgg568@gmail.com>
2023-05-22 14:25:15 +0800

    update menu

--- main.js
+++ main.js
@@ -12,4 +12,21 @@ document.body.addEventListener("click", (e) => {
   }
 });
 
-// Ganti background img
+// Tambah kurang jumlah pesanan
+let tambah = document.getElementById("tambah");
+let kurang = document.getElementById("kurang");
+let banyakPesanan = document.getElementById("banyakPesanan");
+
+let jumlah = 0;
+
+tambah.addEventListener("click", () => {
+  jumlah += 1;
+  banyakPesanan.textContent = jumlah;
+  console.log("bertambah");
+});
+
+kurang.addEventListener("click", () => {
+  jumlah -= 1;
+  banyakPesanan.textContent = jumlah;
+  console.log("berkurang");
+});
